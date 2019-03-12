f7af6c3c3037672ecc8608ab24e7a3502aac8c4b
Anton Matiiv <43201008+AntonMatiiv@users.noreply.github.com>
2019-03-13 00:12:25 +0300

    Update trace_func.py

--- trace_func.py
+++ trace_func.py
@@ -9,6 +9,7 @@ def foo():
 def tracefunc(frame, event, args):
     if event == "return":
         print("functions:", frame.f_code.co_name,",", "local vars:", *frame.f_locals)
+        settrace(None)
     return tracefunc
 
 settrace(tracefunc)
